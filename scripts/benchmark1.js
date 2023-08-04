dc994c27275b8ab7ab08b64e5b56aa53dfb0ee05
ni-eric <nihaitian@gmail.com>
2023-08-04 16:00:55 -0400

    benchmark1 done

--- scripts/benchmark1.js
+++ scripts/benchmark1.js
@@ -14,7 +14,7 @@ async function main() {
     let consents = JSON.parse(fs.readFileSync(path.join("./Release", "consents", folder.toString(), "training_data.json")))
 
     let researcherqueries = JSON.parse(fs.readFileSync(path.join("./Release", "test_meaningful_researcher_queries.json")))
-    let patientqueries = JSON.parse(fs.readFileSync(path.join("./Release", "test_patient_queries.json")))
+    let patientqueries = JSON.parse(fs.readFileSync(path.join("./Release", "test_patient_queries2.json")))
 
     console.time("insertion")
     for (let [i, consent] of tdqm(consents.entries(), { total: 3689 })) {
@@ -29,30 +29,26 @@ async function main() {
     console.timeEnd("insertion")
 
     console.time("researcher query")
-    for (let [i, rquery] of tdqm(researcherqueries.entries(), { total: 50 })) {
+    for (let [i, rquery] of tdqm(researcherqueries.entries(), { total: 1000 })) {
         patientIDs = await deployedContract.queryForResearcher(
             rquery["studyID"],
             rquery["timestamp"],
             rquery["categorySharingChoices"],
             rquery["elementSharingChoices"]
         )
-        const regularIntList = patientIDs.map((bigintValue) => parseInt(bigintValue.toString()))
-        console.log(regularIntList)
+        // const regularIntList = patientIDs.map((bigintValue) => parseInt(bigintValue.toString()))
+        // console.log(regularIntList)
     }
     console.timeEnd("researcher query")
     const regularIntList = patientIDs.map((bigintValue) => parseInt(bigintValue.toString()))
     console.log(regularIntList)
 
     console.time("patient query")
-    // outputstring = await deployedContract.queryForPatient(
-    //     patientqueries[0]["patientID"],
-    //     patientqueries[0]["studyID"],
-    //     patientqueries[0]["startTimestamp"],
-    //     patientqueries[0]["endTimestamp"]
-    // )
-    outputstring = await deployedContract.queryForPatient(8837, 3, 1641024390, 1641047888)
+    for (let [i, pquery] of tdqm(patientqueries.entries(), { total: 1000 })) {
+        outputstring = await deployedContract.queryForPatient(pquery["patientID"], pquery["studyID"], pquery["startTimestamp"], pquery["endTimestamp"])
+        // console.log(outputstring)
+    }
     console.timeEnd("patient query")
-    console.log(outputstring)
 
     const used = process.memoryUsage().heapUsed / 1024 / 1024
     console.log(`The script uses approximately ${Math.round(used * 100) / 100} MB`)
